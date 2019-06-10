c6bcdd6a00c9e9ce95b17bdfc0cfef22cb4a12c9
moaalaa <moaalaa16@gmail.com>
2019-06-10 03:59:32 +0200

    fix paths

---
+++ assets/js/app.js
@@ -0,0 +1,57 @@
+let scene, camera, renderer;
+
+const init = () => {
+    scene = new THREE.Scene;
+
+    // Setup Camera
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000);
+    camera.position.set(-900,-200,-900);
+
+    // Setup Renderer And Append It To The DOM
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // Setup Controls
+    let controls = new THREE.OrbitControls(camera);
+    controls.addEventListener('change', renderer);
+    controls.minDistance = 500;
+    controls.maxDistance = 1500;
+
+    // Setup Material
+    let materialArray = [];
+    let texture_ft = new THREE.TextureLoader().load('../img/valley_ft.jpg');
+    let texture_bk = new THREE.TextureLoader().load('../img/valley_bk.jpg');
+    let texture_up = new THREE.TextureLoader().load('../img/valley_up.jpg');
+    let texture_dn = new THREE.TextureLoader().load('../img/valley_dn.jpg');
+    let texture_rt = new THREE.TextureLoader().load('../img/valley_rt.jpg');
+    let texture_lf = new THREE.TextureLoader().load('../img/valley_lf.jpg');
+
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_lf}));
+    
+    for (let index = 0; index < materialArray.length; index++) {
+        materialArray[index].side = THREE.BackSide;
+    }
+    
+    // Setup Sky box Geometry
+    let skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
+    let skybox = new THREE.Mesh(skyboxGeo, materialArray);
+
+    scene.add(skybox);
+    console.log(scene);
+    
+    animate();
+};
+
+const animate = () => {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(animate);
+};
+
+init();
